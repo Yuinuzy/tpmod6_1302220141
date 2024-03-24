132f650e1f4959521c24fa3000f3bf4b81d5e9cb
Yuinuzy <tsaqif@student.telkomuniversity.ac.id>
2024-03-24 22:16:51 +0700

    implementasi design by contract

---
+++ tpmod6_130220141/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using tpmod6_130220141.SayaTubeVideo;
+
+namespace program;
+class Progam
+{
+
+    public static void Main(String[] Args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("", 0);
+        for (int i = 0; i < 220; i++)
+        {
+            video.incrementPlayCount(10000000);
+            video.printVideoDetails();
+        }
+        Console.WriteLine("max play count :" + int.MaxValue);
+
+    }
+}
